35037cf39b5af27a14c3875bb1753337f40327f0
Tulio Leao <tupaschoal@gmail.com>
2020-03-25 14:02:15 -0300

    Speed up Travis CI and improve error reports

---
+++ script/check_files_formatting.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+# Validate all files adhere to .editorconfig
+# Exclude files which should not be checked against .editorconfig
+# (eg. images, LICENSE, system-generated files, and library files)
+mapfile -t editorconfigargs < <(
+    find . \
+        -type 'f' \
+        ! -name '*.png' \
+        ! -name '*.ico' \
+        ! -name 'LICENSE' \
+        ! -name 'Gemfile.lock' \
+        ! -path './assets/*/libs/*' \
+        ! -path './.bundle/*' \
+        ! -path './.git/*' \
+        ! -path './_site/*' \
+        ! -path './vendor/*' \
+)
+npx @htmlacademy/editorconfig-cli "${editorconfigargs[@]}"
---
+++ script/run_jekyll.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+# Build site, validate HTML
+bundle exec jekyll build
+bundle exec htmlproofer ./_site --checks-to-ignore 'LinkCheck'
