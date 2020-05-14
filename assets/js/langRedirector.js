8aaca252db0b0d72afed079808f631d41d12118d
AHOHNMYC <24810600+AHOHNMYC@users.noreply.github.com>
2020-05-15 01:48:18 +0300

    Add simple redirector Rename existing langs according RFC and ISO: https://tools.ietf.org/html/rfc5646#section-2.1 https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes

---
+++ assets/js/langRedirector.js
@@ -0,0 +1,45 @@
+// Wrap all this to not pollute global namespace
+{
+    // Generate an array of languages from page
+    // TODO: is there a way to do it static using Jekyll?
+    const JDM_LANGS = [...document.querySelectorAll('#language-dropdown > ul > li')].map(el => el.classList[0]);
+
+
+    function setLanguage(lang) {
+        path = lang;
+        // English is mounted on '/'
+        if (lang === 'en')
+            path = '';
+
+        location = location.origin+'/'+path;
+    }
+
+
+    function _tryLanguage(lang) {
+        if (JDM_LANGS.includes(lang))
+            setLanguage(lang);
+    }
+
+
+    function tryLanguage(lang) {
+        _tryLanguage(lang);
+    
+        // Test first two chars. Maybe we have not local, but more common language?
+        lang = lang.slice(0,2);
+        _tryLanguage(lang);
+    }
+
+
+    function tryLanguages() {
+        // We don't have to handle returns from `tryLanguage' because of
+        // this function does redirect if language considered good
+        navigator.languages.forEach(tryLanguage);
+    }
+
+
+    // Do redirection only one time
+    if (!localStorage.getItem('langRedirectionHasDone')) {
+        localStorage.setItem('langRedirectionHasDone', true);
+        tryLanguages();
+    }
+}
