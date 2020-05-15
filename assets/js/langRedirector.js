c6f8b59298bb044d06b8afb54ad1ba5ba443b76e
AHOHNMYC <24810600+AHOHNMYC@users.noreply.github.com>
2020-05-15 13:37:27 +0300

    Rewrite redirector to simplify it

--- assets/js/langRedirector.js
+++ assets/js/langRedirector.js
@@ -1,45 +1,36 @@
 // Wrap all this to not pollute global namespace
 {
     // Generate an array of languages from page
-    // TODO: is there a way to do it static using Jekyll?
     const JDM_LANGS = [...document.querySelectorAll('#language-dropdown > ul > li')].map(el => el.classList[0]);
 
 
-    function setLanguage(lang) {
-        path = lang;
-        // English is mounted on '/'
-        if (lang === 'en')
-            path = '';
-
-        location = location.origin+'/'+path;
+    function redirectTo(lang) {
+        const link = document.querySelector(`#language-dropdown > ul > li.${lang} > a`);
+	if (link)
+            link.click();
     }
 
 
-    function _tryLanguage(lang) {
-        if (JDM_LANGS.includes(lang))
-            setLanguage(lang);
-    }
+    function guessedTranslation() {
+        const langs = navigator.languages;
+        for (let i=0; i < langs.length; i++) {
+            let lang = langs[i];
 
+            if (JDM_LANGS.includes(lang))
+                return lang;
 
-    function tryLanguage(lang) {
-        _tryLanguage(lang);
-
-        // Test first two chars. Maybe we have not local, but more common language?
-        lang = lang.slice(0,2);
-        _tryLanguage(lang);
-    }
-
-
-    function tryLanguages() {
-        // We don't have to handle returns from `tryLanguage' because
-        // this function already redirects if a language is considered good
-        navigator.languages.forEach(tryLanguage);
+            // Test 'ru' in addition to 'ru-RU'
+            lang = lang.slice(0,2);
+            if (JDM_LANGS.includes(lang))
+                return lang;
+        }
+        return null;
     }
 
 
     // Do redirection only one time
     if (!localStorage.getItem('langRedirectionHasDone')) {
         localStorage.setItem('langRedirectionHasDone', true);
-        tryLanguages();
+        redirectTo(guessedTranslation());
     }
 }
