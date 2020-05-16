b9fae2e177c677c3c2c62839685b8c01236fc30a
AHOHNMYC <24810600+AHOHNMYC@users.noreply.github.com>
2020-05-16 14:57:56 +0300

    Do all the tests firstly on first languages Instead of first test on all languages

--- assets/js/langRedirector.js
+++ assets/js/langRedirector.js
@@ -14,30 +14,22 @@
     function guessedTranslation() {
         const langs = navigator.languages;
 
-        // Every cycle is filter. It returns value if something was catched.
-        // Thus, they are ordered according to the user's wishes: from more strict to less strict
-
-        // user has: 'pt-BR'
-        // we have:  'pt-BR'
         for (let i=0; i < langs.length; i++) {
             let lang = langs[i];
+
+            // user has: 'pt-BR'
+            // we have:  'pt-BR'
             if (JDM_LANGS.includes(lang))
                 return lang;
-        }
 
-        // user has: 'ru-RU'
-        // we have:  'ru'
-        for (let i=0; i < langs.length; i++) {
-            let lang = langs[i];
+            // user has: 'ru-RU'
+            // we have:  'ru'
             lang = lang.slice(0,2);
             if (JDM_LANGS.includes(lang))
                 return lang;
-        }
 
-        // user has: 'pt-AO' or 'pt'
-        // we have:  'pt-BR'
-        for (let i=0; i < langs.length; i++) {
-            let lang = langs[i].slice(0,2);
+            // user has: 'pt-AO' or 'pt'
+            // we have:  'pt-BR'
             for (let j=0; j < JDM_LANGS.length; j++) {
                 let jdm_lang = JDM_LANGS[j];
                 if (jdm_lang.slice(0,2) === lang)
