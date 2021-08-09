55f5cdce7f9370d707d5fcf26a06480a269d2e7d
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-08-08 19:09:57 -0600

    Add 'search' key to JSON validator and translation reference

--- script/validate_json.rb
+++ script/validate_json.rb
@@ -27,7 +27,7 @@ SupportedLanguageKeys = ["about", "contribute", "difficulty", "difficulty_easy",
                         "extensionp3", "extensionp4", "extensionp5", "extensionp6", "footercredits",
                         "guide", "guideeasy", "guideexplanations", "guidehard", "guideimpossible",
                         "guidemedium", "hideinfo", "jgmd", "mikerogers", "name", "noinfo", "noresults", "noresultshelp",
-                        "popular", "pullrequest", "reset", "sendmail", "showinfo", "tagline", "title", "twitter", "whatisthis",
+                        "popular", "pullrequest", "reset", "search", "sendmail", "showinfo", "tagline", "title", "twitter", "whatisthis",
                         "whatisthis1", "whatisthis2", "whatisthis3", "whatisthis4"]
 
 def get_supported_languages()
